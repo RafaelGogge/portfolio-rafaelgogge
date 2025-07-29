eb7aaf2281b30226cd21910134ca623fad0fd376
Raffa <165601931+RafaelGogge@users.noreply.github.com>
2025-07-29 15:58:06 -0300

    Update alert.tsx

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-xl border-2 border-transparent p-4 bg-gradient-to-br from-warning/10 via-primary/10 to-accent/10 shadow-lg transition-all duration-300 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground hover:border-warning/60 hover:shadow-2xl",
   {
     variants: {
       variant: {
